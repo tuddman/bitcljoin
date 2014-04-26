60cbba58b61be862e97f95c890376345ba90cb49
tuddman <scott.tudd@gmail.com>
2014-04-26 17:29:41 -0400

    update to 11.2 & minor cleanup.

--- src/bitcoin/channels.clj
+++ src/bitcoin/channels.clj
@@ -3,13 +3,16 @@
   (:require [lamina.core :as lamina]
             [bitcoin.core :as btc]))
 
+
 (defn txs->maps
   [ch]
   (lamina/filter* btc/tx->map ch))
 
+
 (defn blocks->maps
   [ch]
   (lamina/filter* btc/block->map ch))
+
 
 (defn broadcast-txs
   "Returns a of new transactions being broadcast (not yet in the blockchain)"
@@ -18,6 +21,7 @@
      (btc/on-tx-broadcast pg (fn [_ tx] (lamina/enqueue ch tx)))
      ch))
   ([] (broadcast-txs @btc/current-pg)))
+
 
 (defn txs-for
   "Creates a channel for transactions sent to a specific address
@@ -30,6 +34,7 @@
     (lamina/filter* #((btc/to-addresses %) address) channel))
   ([address]
    (txs-for (broadcast-txs) address)))
+
 
 (defn wallet-txs
   "Creates a channel for transactions sent to a specific address
@@ -56,6 +61,7 @@
      ch))
   ([] (confirmed-txs @btc/current-bc)))
 
+
 (defn blocks
   "Returns a of new blocks entering the blockchain"
   ([bc]
@@ -68,20 +74,25 @@
      ch))
   ([] (blocks @btc/current-bc)))
 
+
 (defn txs->maps
   [ch]
   (lamina/map* btc/tx->map ch))
 
+
 (defn blocks->maps
   [ch]
   (lamina/map* btc/block->map ch))
+
 
 (defn log-errors
   "Log all errors"
   [ch]
   (lamina/on-error ch #(print-cause-trace %)))
 
+
 (defn prn-all
   "prn content of channel"
   [ch]
   (lamina/receive-all ch prn))
+
